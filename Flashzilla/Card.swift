7f6fdbd2c33fc2be1b93b845cc3fc49a83c77bde
Issac Penn <pbbsignup@outlook.com>
2019-12-22 08:55:44 +0800

    Day 88 done

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Issac Penn on 2019/12/22.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static var example: Card {
+        return Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
